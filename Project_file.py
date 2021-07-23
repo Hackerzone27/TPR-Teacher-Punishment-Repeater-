8e846963d8efcaaf360bfb344e981797c7b0c54c
Hackerzone27 <80245494+Hackerzone27@users.noreply.github.com>
2021-07-23 10:23:52 +0530

    Add files via upload

---
+++ Project_file.py
@@ -0,0 +1,16 @@
+#This is the start of the program
+
+print("")
+print("Welcome, we are your friend who'll help u escape from your teacher's given punishment.")
+print("")
+
+x=0                                                         #This is a temporary variable.
+y=input("What is your punishment? ")                        #We can enter our punishment here.
+z=input("How many times u have to repeat? ")                #We can enter the Number of times to loop the statement
+
+#Here punishment y is looped z times
+
+for x in range(int(z)):
+    print(y)
+
+#This is the start of the program
